ba682b56bb0a669a5cb4d7f668d8363564421fc6
muhfarian <12150114744@students.uin-suska.ac.id>
2023-03-28 14:25:25 +0700

    Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYBvYhb1jcsxvUN6ZZO1VWZ0w_C1R2XrpOQ&usqp=CAU" width="1000" height="200" />
+
+
+    <div class="wrapper">
+      <HelloWorld msg="Ini Web" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+  div{
+    font-family: monospace;
+    font-size: 500%;
+  }
+</style>
